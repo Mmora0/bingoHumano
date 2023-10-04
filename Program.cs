3dde927bc666a2ff6017db305486fe8848db2fe7
Miguel Angel Mora Ortiz <125917913+Mmora0@users.noreply.github.com>
2023-10-04 16:04:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿//Sabe nadar
+
+string nada;
+
+Console.WriteLine("Quién sabe nadar?");
+nada = Console.ReadLine();
+
+Console.WriteLine(nada + " sabe nadar.");
+
+//Color favorito
+
+string color;
+
+Console.WriteLine("Quién tiene el mismo gusto de color?");
+color = Console.ReadLine();
+
+Console.WriteLine("A " + color + " le gusta mi mismo color.");
+
+//No le gusta el fútbol
+
+string futbol;
+
+Console.WriteLine("A quién no le gusta el fútbol?");
+futbol = Console.ReadLine();
+
+Console.WriteLine("A " + futbol + " no le gusta el fútbol");
+
+//Le gusta hacer deporte
+
+string deporte;
+
+Console.WriteLine("A quién le gusta hacer deporte?");
+deporte = Console.ReadLine();
+
+Console.WriteLine("A " + deporte + " le gusta hacer deporte.");
+
+//Nació el mismo mes
+
+string mes;
+
+Console.WriteLine("Quién nació en su mismo mes de nacimiento?");
+mes = Console.ReadLine();
+
+Console.WriteLine(mes + " y yo nacimos en el mismo mes.");
+
+//Le gusta el buñuelo
+
+string buñuelo;
+
+Console.WriteLine("A quién le gusta comer buñuelo?");
+buñuelo = Console.ReadLine();
+
+Console.WriteLine("A " + buñuelo + " le gusta comer buñuelo.");
+
+//Tiene el mismo pasatiempo
+
+string patiempo;
+
+Console.WriteLine("Quién tiene mi mismo pasatiempo?");
+patiempo = Console.ReadLine();
+
+Console.WriteLine(patiempo + " tiene mi mismo pasatiempo.");
+
+//Le gustan los niños
+
+string niños;
+
+Console.WriteLine("A quién le gustan los niños?");
+niños = Console.ReadLine();
+
+Console.WriteLine("A " + niños + " le gustan los niños.");
+
+//Le gusta la matemática
+
+string mates;
+
+Console.WriteLine("A quién le gustan las matemáticas?");
+mates = Console.ReadLine();
+
+Console.WriteLine("A " + mates + " le gustan las matemáticas");
+
+//Tiene ojos oscuros
+
+string ojos;
+
+Console.WriteLine("Quién tiene los ojos oscuros?");
+ojos = Console.ReadLine();
+
+Console.WriteLine(ojos + " tiene los ojos oscuros.");
+
+//le gusta el baile
+
+string baile;
+
+Console.WriteLine("A quién le gusta el baile?");
+baile = Console.ReadLine(); 
+
+Console.WriteLine("A " + baile + " le gusta bailar.");
+
+//hijo único
+
+string mijo;
+
+Console.WriteLine("Quién es hijo único?");
+mijo = Console.ReadLine();
+
+Console.WriteLine(mijo + " es hijo único");
+
+//le gusta la natilla
+
+string natilla;
+
+Console.WriteLine("A quién le gusta la natilla?");
+natilla = Console.ReadLine();
+
+Console.WriteLine("A " + natilla + " le gusta la natilla.");
+
+//le gusta leer
+
+string leer;
+
+Console.WriteLine("A quién le gusta leer?");
+leer = Console.ReadLine();
+
+Console.WriteLine("A " + leer + " le gusta leer.");
+
+//tiene familia en otra familia
+
+string familia;
+
+Console.WriteLine("Quién tiene familia en otra ciudad?");
+familia = Console.ReadLine();
+
+Console.WriteLine(familia + " tiene familia en otra ciudad.");
+
+//le gusta investigar
+
+string invest;
+
+Console.WriteLine("A quién le gusta investigar?");
+invest = Console.ReadLine();
+
+Console.WriteLine("A " + invest + " le gusta investigar.");
+
+//no es del mismo sexo
+
+string sexo;
+
+Console.WriteLine("Hay aguien que no sea de tu mismo sexo?");
+Console.WriteLine("Todos son del mismo sexo");
+
+//ha ganado algo
+
+string ganar;
+
+Console.WriteLine("Alguien que se haya ganado algo.");
+ganar = Console.ReadLine();
+
+Console.WriteLine(ganar + " ha ganado algo.");
+
+//Tiene hijo menor de 18
+
+string hijo;
+
+Console.WriteLine("Hay alguien que tenga un hijo menor de 18 años?");
+Console.WriteLine("No hay alguien que tenga un hijo.");
+
+Console.WriteLine("Los entrevistados fueron: " + nada + ", " + color + ", " + futbol + ", " + deporte + ", " + mes + ", " + buñuelo + ", " + patiempo + ", " + niños + ", " + mates + ", " + ojos + ", " + baile + ", " + mijo + ", " + natilla + ", " + leer + ", " + familia + ", " + invest + " y " + ganar);
